a0f126b7780c5e82438cf37fffe4884a2d135a0b
Rollphes <>
2021-12-04 18:08:36 +0900

    first commit

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-test
+import fs from 'fs'
+import request from 'request'
+
+const url =
+  'https://www.mirrativ.com/api/live/live?live_id=' +
+  process.argv[2].split(/\//)[4]
+
+request({ method: 'GET', url: url }, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    console.log(JSON.parse(body))
+  }
+})
