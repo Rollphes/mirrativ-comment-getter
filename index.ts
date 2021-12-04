fdc920473ff9d7bd0c831653c8a194955a4e92f5
Rollphes <>
2021-12-04 21:33:56 +0900

    coping with url handler

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ let mirrativ: Mirrativ
 
 const url =
   'https://www.mirrativ.com/api/live/live?live_id=' +
-  process.argv[2].split(/\//)[4]
+  process.argv[2].split(/[\/|?]/)[4]
 request({ method: 'GET', url: url }, function (error, response, body) {
   if (!error && response.statusCode === 200) {
     mirrativ = JSON.parse(body) as Mirrativ
